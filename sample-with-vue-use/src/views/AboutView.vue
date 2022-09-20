38e6332ab5379699ce8bd3df0ae1132fb0311440
Rajitha Kumara <rajitha@orangehrm.us.com>
2022-09-20 14:45:08 +0530

    [sample-with-vue-use] add draggable

--- sample-with-vue-use/src/views/AboutView.vue
+++ sample-with-vue-use/src/views/AboutView.vue
@@ -1,6 +1,9 @@
 <script setup lang="ts">
-import { useTitle } from "@vueuse/core";
-import { onUnmounted } from "vue";
+import { useTitle,useDraggable } from "@vueuse/core";
+import { onBeforeUnmount,ref } from "vue";
+
+const el = ref<HTMLElement | null>(null)
+const { x, y, style } = useDraggable(el)
 
 const title = useTitle();
 const currentTitle = "Vue App";
@@ -14,7 +17,7 @@ const toggleTitle = () => {
 };
 
 const interval = setInterval(toggleTitle, 1000);
-onUnmounted(async () => {
+onBeforeUnmount(async () => {
   clearInterval(interval);
   title.value = currentTitle;
 });
@@ -23,6 +26,9 @@ onUnmounted(async () => {
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+  </div>
+  <div ref="el" :style="style" style="position: fixed" class="cursor-pointer p-6 bg-emerald-500 rounded-full select-none">
+    👋 Drag me! I am at {{x}}, {{y}}
   </div>
 </template>
 
